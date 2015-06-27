a5af969b35cb42b03657e99e3a989b92e8ed0a9d
bryanelidimas <bryanelidimas@gmail.com>
2015-06-27 17:58:38 -0500

    Add ability to sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    http_response = Http.post("http://localhost:3000/signin.json", json: { email: params[:session][:email], password: params[:session][:password] })
+
+    if http_response.code == 200
+      user = JSON.parse(http_response)
+      session[:email] = user["email"]
+
+      redirect_to sign_in_path, notice: "Signed..... In.😎"
+    else
+      flash.now[:alert] = "Couldn't log ya in :/."
+    end
+
   end
 
   def delete
+    session.delete :email
+    redirect_to sign_in_path
   end
 end
