483e61e6a5fb21564cd680a096704a708bb3af77
bryanelidimas <bryanelidimas@gmail.com>
2015-06-27 19:07:05 -0500

    Fix routes and Add ability to show Secrets

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,6 @@
 class SecretsController < ApplicationController
   def index
+    @secrets = JSON.parse Http.headers("api-key" => "876bd5798a6fe2d3").get("http://localhost:3000/secrets/all.json")
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root 'signin' => 'sessions#new', as: :sign_in
+  root 'secrets#index'
 
-  get '/' => 'secrets#index', as: :index_path
   get 'new_secret' => 'secrets#new', as: :new_secret
   post 'secrets' => 'secrets#create', as: :secrets
 
   get   'signup' => 'users#new', as: :sign_up
   post  'signup' => 'users#create', as: :users
 
+  get 'signin' => 'sessions#new', as: :sign_in
   post 'auth' => 'sessions#create', as: :auth
   delete 'signout' => 'sessions#delete', as: :sign_out
 end
