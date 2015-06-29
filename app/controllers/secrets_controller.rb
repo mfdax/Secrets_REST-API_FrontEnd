a1e64b2507258d1ec6d25dcbf165a17906c5392a
bryanelidimas <bryanelidimas@gmail.com>
2015-06-29 16:15:03 -0500

    Update to match the backend controller

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -7,5 +7,12 @@ class SecretsController < ApplicationController
   end
 
   def create
+    http_response = Http.headers("api-key" => "sup3r53cure4p1k3y").post("http://localhost:3000/newsecret.json", json: {secret: {content: params[:secret][:content]}})
+
+    if http_response.code == 201
+      redirect_to root_path, notice: "Posted new secret! :)"
+    else
+      flash.now[:alert] = "Nahh. Not working"
+    end
   end
 end
